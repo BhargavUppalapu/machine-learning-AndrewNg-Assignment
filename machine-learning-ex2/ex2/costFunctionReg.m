f5738a32d0d0064133fd41ffacea70f6e414a026
Bhargav <bhargav.iiith@gmail.com>
2015-08-12 12:55:27 +0530

    Added Cost Function and Gradiant decent

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -19,6 +19,14 @@ grad = zeros(size(theta));
 
 htheta = sigmoid(X*theta);
  J = ( (1/m)  *  sum(((y.*-1) .* log(htheta)) - ((1.-y) .* log(1.-htheta))) ) +  ((lambda/2*m)*sum(theta.*theta));
+error = htheta - y;
+
+grad = ((1/m)) * ((X' * error));
+
+
+regularization = zeros(size(theta));
+regularization(2:size(regularization))  = ((lambda/m) .* theta(2:size(theta)));
+grad = grad + regularization;
 
 
 % =============================================================
