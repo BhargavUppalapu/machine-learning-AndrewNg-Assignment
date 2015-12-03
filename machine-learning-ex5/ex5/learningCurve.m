a3fee94c15a1f7eeb9d94ac606b0d51e07fc23c3
Bhargav <bhargav.iiith@gmail.com>
2015-12-03 15:34:57 +0530

    Ex-5

--- machine-learning-ex5/ex5/learningCurve.m
+++ machine-learning-ex5/ex5/learningCurve.m
@@ -61,9 +61,6 @@ for i = 1:m
 	%lambda = 0;
 	[theta] = trainLinearReg(X(1:i,:), y(1:i), lambda);
 	error_train(i) = linearRegCostFunction(X(1:i,:), y(1:i), theta, 0);
-	
-	%theta_validation = [1;1];
-	%[theta_validation] = trainLinearReg(Xval, yval, lambda);
 	error_val(i) = linearRegCostFunction(Xval, yval, theta,0 );
 end
 
